b8fa0b7f53701958f97a174ba75be710d28cfffa
Artsiom Mezin <iketari@gmail.com>
2018-08-12 09:44:18 +0200

    change detection

--- src/app/shared/components/thread-preview/thread-preview.component.ts
+++ src/app/shared/components/thread-preview/thread-preview.component.ts
@@ -1,13 +1,18 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { IThreadSearchResult } from '../../models/search';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
+import { IThreadSearchResult, ISearchResult } from '../../models/search';
 import { IEmail } from '../../models/message';
 
 @Component({
   selector: 'thread-preview',
   templateUrl: './thread-preview.component.html',
-  styleUrls: ['./thread-preview.component.scss']
+  styleUrls: ['./thread-preview.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ThreadPreviewComponent {
   @Input()
   thread: IThreadSearchResult<IEmail>;
+
+  public trackByFn(_index: number, item: ISearchResult<IEmail>) {
+    return item.originalItem.id;
+  }
 }
